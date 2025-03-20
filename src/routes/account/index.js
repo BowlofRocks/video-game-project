b464ff1edbaa54a77ab040c1f96cb253a5188557
Paul Amago <bowlofrocks@users.noreply.github.com>
2025-03-20 15:34:06 -0600

    login routes and views implemented

--- src/routes/account/index.js
+++ src/routes/account/index.js
@@ -4,7 +4,17 @@ const router = Router();
 
 // The home page route
 router.get("/", async (req, res) => {
-  res.render("index", { title: "Account Page" });
+  res.render("account/index", { title: "Account Page" });
+});
+
+// The login page route
+router.get("/login", async (req, res) => {
+  res.render("account/login", { title: "Account Page" });
+});
+
+// The register page route
+router.get("/register", async (req, res) => {
+  res.render("account/register", { title: "Account Page" });
 });
 
 export default router;
